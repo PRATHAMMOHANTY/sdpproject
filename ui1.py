7af141adc83550f4a7b2eec79c310a35f1c80fa5
MOHANTY PRATHAM <87562297+PRATHAMMOHANTY@users.noreply.github.com>
2024-05-16 12:50:44 +0530

    Update ui1.py

--- ui1.py
+++ ui1.py
@@ -104,7 +104,7 @@ def calculate_flooded_percentage(image):
 uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    model_absolute_path = os.path.abspath("sdpproject/model.h5")
+    model_absolute_path = os.path.abspath("/mount/src/sdpproject/sdpproject/model.h5")
     model = load_model(model_absolute_path)
     # Read the image
     image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
